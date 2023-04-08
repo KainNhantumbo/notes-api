85e808d61f6a397f477b657ee1bc6f9212edce86
Kain Nhantumbo <nhantumbok@gmail.com>
2023-04-08 21:48:38 +0200

    dev: added cloudnary config

---
+++ src/config/cloudnary.ts
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary';
+import * as dotenv from 'dotenv';
+
+// reads the dotenv file
+dotenv.config();
+
+// Configuration
+cloudinary.config({
+  secure: true,
+  use_filename: true,
+  unique_filename: false,
+  overwrite: true,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinaryUploader = (imageData: string) =>
+  cloudinary.uploader.upload(imageData, {
+    folder: 'PDF-API-USERS',
+  });
+
+export { cloudinaryUploader, cloudinary };
