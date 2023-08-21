69778425229a15ab37382153f0134cdaf0242fda
Kain Nhantumbo <nhantumbok@gmail.com>
2023-08-21 13:34:26 +0200

    chore: removed some unused packages.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import express from 'express';
 import helmet from 'helmet';
 import Bootstrap from './modules/app';
+import compression from 'compression';
 import ErrorHandler from './lib/error-handler';
 import cookieParser from 'cookie-parser';
-import { config } from 'dotenv';
+import * as dotenv from 'dotenv';
 import { corsOptions } from './config/cors';
 import { rateLimiter } from './config/rate-limiter';
 import { notFoundRoute } from './routes/404';
 
 //server configuration
-config();
+dotenv.config();
 const app = express();
 const dbUri = process.env.DB_URI || '';
 const PORT = Number(process.env.DB_URI) || 5700;
+
 
 // middlewares
 app.use(helmet());
 app.use(corsOptions);
 app.use(rateLimiter);
+app.use(compression());
 app.use(express.json({ limit: '100mb' }));
 app.use(cookieParser());
 
+// routes
 app.use(notFoundRoute);
 app.use(ErrorHandler.handler);
 
