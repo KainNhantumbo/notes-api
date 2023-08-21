c7ce67aeda434bcd6bcc58c41229e438da14feef
Kain Nhantumbo <nhantumbok@gmail.com>
2023-08-21 21:18:15 +0200

    feat: created notes routes.

---
+++ src/routes/notes.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import asyncWrapper from '../lib/async-wrapper';
+import authenticate from '../middlewares/auth';
+import NotesController from '../controllers/notes.controller';
+
+const router = Router();
+const controller = new NotesController();
+
+router
+  .route('/')
+  .get(authenticate, asyncWrapper(controller.getAllNotes))
+  .post(asyncWrapper(controller.createNote));
+
+router
+  .route('/:id')
+  .get(authenticate, asyncWrapper(controller.getNote))
+  .patch(authenticate, asyncWrapper(controller.updateNote))
+  .delete(authenticate, asyncWrapper(controller.deleteNote));
+
+export { router as notesRoutes };
