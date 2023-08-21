c7cdb1b772df1983786927ef5fd52a20d0b28f48
Kain Nhantumbo <nhantumbok@gmail.com>
2023-08-21 14:25:56 +0200

    feat: added user model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,14 @@
 import * as bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
+import { validateEmail } from '../lib/validators';
 
 interface IUser {
   first_name: string;
   last_name: string;
   email: string;
-  picture: { id: String; url: String };
+  profile_image: { id: string; url: string };
   password: string;
-  recovery_code: { code: string; date: string };
-  subscription: { type: string; exp_date: string; issued_date: string };
+  last_session: Date;
 }
 
 const UserSchema = new Schema<IUser>(
@@ -17,39 +17,33 @@ const UserSchema = new Schema<IUser>(
       type: String,
       trim: true,
       required: [true, 'First name must be provided'],
-      maxlength: [32, 'Provided first name is too long'],
+      maxlength: [32, 'First name field length is too long'],
     },
     last_name: {
       type: String,
       trim: true,
       required: [true, 'Last name must be provided.'],
-      maxlength: [32, 'Provided last name is too long'],
+      maxlength: [32, 'Last name field length is too long'],
     },
     email: {
       type: String,
       required: [true, 'Please provide your e-mail adress'],
       trim: true,
       lowercase: true,
-      match: [
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please provide a valid e-mail adress',
-      ],
+      validate: {
+        validator: validateEmail,
+        message: 'Please provide a valid email adress',
+      },
       unique: true,
-      maxlength: [64, 'Provided e-mail adress is too long'],
+      maxlength: [64, 'E-mail adress field length is too long'],
     },
     password: {
       type: String,
       minlength: [6, 'The password must have at least 6 characters'],
       required: [true, 'Please provide a password'],
     },
-    recovery_code: {
-      code: String,
-      date: String,
-    },
-    picture: {
-      id: String,
-      url: String,
-    },
+    profile_image: { id: String, url: String },
+    last_session: { type: Date, default: Date.now() },
   },
   { timestamps: true }
 );
@@ -65,5 +59,5 @@ UserSchema.pre('save', async function (next) {
   }
 });
 
-const UserModel = model('User', UserSchema);
-export default UserModel;
+const User = model('User', UserSchema);
+export default User;
